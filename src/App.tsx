5a780d591db1f42248fb775b85dd277414054941
Gabriel Borel <biel_borel@hotmail.com>
2022-07-31 17:09:34 -0300

    Project initialized with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl">hahaa</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'blue': '##3772FF',
+      'green': '#36D6AD',
+      'coral': '#FC7071',
+      'coral-hover': '#FF9D9E',
+      'cream': '#FCF0E3',
+      'gray': '#737380',
+      'light-gray': '#BCBCBC',
+      'background-gray': '#F6F6F6'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
